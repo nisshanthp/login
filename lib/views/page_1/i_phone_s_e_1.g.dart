bfa6df42e57f6fa0972b7f9a9adeefce81f33602
Parabeac-Nest <admin@parabeac.com>
2022-06-10 22:47:21 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/views/page_1/i_phone_s_e_1.g.dart
@@ -0,0 +1,349 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class IPhoneSE1 extends StatefulWidget {
+  const IPhoneSE1({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _IPhoneSE1 createState() => _IPhoneSE1();
+}
+
+class _IPhoneSE1 extends State<IPhoneSE1> {
+  _IPhoneSE1();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Color(0xffab1313),
+      child: Stack(children: [
+        Positioned(
+          left: MediaQuery.of(context).size.width * 0.222,
+          width: MediaQuery.of(context).size.width * 0.553,
+          top: 93.0,
+          height: 40.0,
+          child: Image.asset(
+            'assets/images/image1.png',
+            package: 'login',
+            width: MediaQuery.of(context).size.width * 0.553,
+            height: 40.000,
+            fit: BoxFit.fill,
+          ),
+        ),
+        Positioned(
+          left: 18.0,
+          width: 283.0,
+          top: 150.0,
+          height: 267.0,
+          child: Container(
+            width: 283.000,
+            height: 267.000,
+            decoration: BoxDecoration(
+              color: Color(0xfffdfdfd),
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 35.0,
+          width: 248.0,
+          top: 202.0,
+          height: 37.0,
+          child: Container(
+              width: 248.000,
+              height: 37.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 248.0,
+                  top: 0,
+                  height: 37.0,
+                  child: Container(
+                    width: 248.000,
+                    height: 37.000,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                      border: Border.all(
+                        color: Color(0xff941a1a),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 10.941,
+                  width: 14.588,
+                  top: 11.1,
+                  height: 14.8,
+                  child: SvgPicture.asset(
+                    'assets/images/mbriuser1.svg',
+                    package: 'login',
+                    width: 14.588,
+                    height: 14.800,
+                    fit: BoxFit.none,
+                  ),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 35.0,
+          width: 248.0,
+          top: 265.0,
+          height: 37.0,
+          child: Container(
+              width: 248.000,
+              height: 37.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 238.0,
+                  width: 248.0,
+                  top: 234.0,
+                  height: 37.0,
+                  child: Container(
+                    width: 248.000,
+                    height: 37.000,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                      border: Border.all(
+                        color: Color(0xff941a1a),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 248.941,
+                  width: 14.588,
+                  top: 248.1,
+                  height: 14.8,
+                  child: Container(
+                      clipBehavior: Clip.hardEdge,
+                      width: 14.588,
+                      height: 14.800,
+                      decoration: BoxDecoration(),
+                      child: Stack(children: [
+                        Positioned(
+                          left: 0,
+                          width: 512.0,
+                          top: 0,
+                          height: 512.0,
+                          child: Image.asset(
+                            'assets/images/image3.png',
+                            package: 'login',
+                            width: 512.000,
+                            height: 512.000,
+                            fit: BoxFit.none,
+                          ),
+                        ),
+                        Positioned(
+                          left: 0,
+                          width: 512.0,
+                          top: 0,
+                          height: 512.0,
+                          child: Image.asset(
+                            'assets/images/image2.png',
+                            package: 'login',
+                            width: 512.000,
+                            height: 512.000,
+                            fit: BoxFit.none,
+                          ),
+                        ),
+                      ])),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 46.0,
+          width: 15.0,
+          top: 276.0,
+          height: 15.0,
+          child: Image.asset(
+            'assets/images/password621.png',
+            package: 'login',
+            width: 15.000,
+            height: 15.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 35.0,
+          width: 90.0,
+          top: 309.0,
+          height: 12.0,
+          child: Container(
+              width: 90.000,
+              height: 12.000,
+              child: AutoSizeText(
+                'Forgotten password?',
+                style: TextStyle(
+                  fontFamily: 'Adamina',
+                  fontSize: 9,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 69.0,
+          width: 179.0,
+          top: 329.0,
+          height: 23.0,
+          child: Container(
+            width: 179.000,
+            height: 23.000,
+            decoration: BoxDecoration(
+              color: Color(0xffa01616),
+              borderRadius: BorderRadius.all(Radius.circular(100)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 69.0,
+          width: 179.0,
+          top: 385.0,
+          height: 23.0,
+          child: Container(
+            width: 179.000,
+            height: 23.000,
+            decoration: BoxDecoration(
+              color: Color(0xff0f0e0e),
+              borderRadius: BorderRadius.all(Radius.circular(100)),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 109.0,
+          width: 101.0,
+          top: 363.0,
+          height: 11.0,
+          child: Container(
+              width: 101.000,
+              height: 11.000,
+              child: AutoSizeText(
+                'Don’t have an account?',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 9,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 146.0,
+          width: 25.0,
+          top: 335.0,
+          height: 11.0,
+          child: Container(
+              width: 25.000,
+              height: 11.000,
+              child: AutoSizeText(
+                'Login',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 9,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 125.0,
+          width: 69.0,
+          top: 391.0,
+          height: 11.0,
+          child: Container(
+              width: 69.000,
+              height: 11.000,
+              child: AutoSizeText(
+                'Create Account',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 9,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 35.0,
+          width: 152.0,
+          top: 181.0,
+          height: 14.0,
+          child: Container(
+              width: 152.000,
+              height: 14.000,
+              child: AutoSizeText(
+                'Username/Email Address',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 0,
+                  color: Color(0xff151515),
+                  shadows: [
+                    Shadow(
+                      color: Color(0x40000000),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+        Positioned(
+          left: 35.0,
+          width: 61.0,
+          top: 245.0,
+          height: 16.0,
+          child: Container(
+              width: 61.000,
+              height: 16.000,
+              child: AutoSizeText(
+                'Password',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 0,
+                  color: Color(0xff151515),
+                  shadows: [
+                    Shadow(
+                      color: Color(0x40000000),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
